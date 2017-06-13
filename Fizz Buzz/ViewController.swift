d176e596864cdf7caf4b77f4ff383b758a404489
Ananya Sehgal <asehgal@ananyas-macbook-air.local>
2017-06-13 09:57:57 -0500

    Initial Commit

--- Fizz Buzz/ViewController.swift
+++ Fizz Buzz/ViewController.swift
@@ -10,16 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myNumberLabel: UILabel!
+    var number = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
+        print("Screen Tapped")
+        number += 1
+        myNumberLabel.text = "\(number)"
+        
+        if number % 15 == 0 {
+            
+           print("Fizz Buzz")
+            
+            view.backgroundColor = UIColor.cyan
+            
+            myNumberLabel.text = "Fizz Buzz"
+            
+            myNumberLabel.font = myNumberLabel.font.withSize(75)
+            
+            
+            
+        }
+        
+        else if number % 3 == 0 {
+            
+            print("Fizz")
+            
+            view.backgroundColor = UIColor.blue
+            
+            myNumberLabel.text = "Fizz"
+            
+            myNumberLabel.font = myNumberLabel.font.withSize(100)
+            
+            
+            
+        }
+        
+        else if number % 5 == 0 {
+            
+            print("Buzz")
+            
+            view.backgroundColor = UIColor.red
+            
+            myNumberLabel.text = "Buzz"
+            
+        myNumberLabel.font = myNumberLabel.font.withSize(100)
+            
+        }
+            
+            else {
+                
+                myNumberLabel.text = "\(number)"
+            
+        myNumberLabel.font = myNumberLabel.font.withSize(100)
+            
+            view.backgroundColor = UIColor.black
+                
+            }
+        }
+    @IBAction func longPress(_ sender: Any) {
+        
+        number = 0
+        myNumberLabel.text = "\(number)"
+        view.backgroundColor = UIColor.black
+        myNumberLabel.font = myNumberLabel.font.withSize(100)
+        
+        print("Reset")
     }
-
+    
+    
 
 }
+
 
